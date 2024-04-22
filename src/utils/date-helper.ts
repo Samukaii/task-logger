51798cbfa593e2252c89fb9c950df31ef7799219
samuel <samuel.veras@joinin.com.br>
2024-04-21 23:45:48 -0300

    chore: add npm publish script

--- src/utils/date-helper.ts
+++ src/utils/date-helper.ts
@@ -1,9 +1,13 @@
 
 const isAfter = (first: Date, second: Date) => first.getTime() > second.getTime();
+const isAfterOrEqual = (first: Date, second: Date) => first.getTime() < second.getTime();
 const isBefore = (first: Date, second: Date) => first.getTime() < second.getTime();
+const isBeforeOrEqual = (first: Date, second: Date) => first.getTime() < second.getTime();
 
 
 export const dateHelper = {
     isAfter,
-    isBefore
+    isBefore,
+    isBeforeOrEqual,
+    isAfterOrEqual
 }
